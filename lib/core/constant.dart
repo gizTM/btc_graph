0836c33305cf19c660245a282b12a85d0d9db1fe
Tharinee Munkwamdee <tharinee_fang@hotmail.com>
2025-01-27 14:38:28 +0700

    fix api retrieval interval time to 30 seconds

--- lib/core/constant.dart
+++ lib/core/constant.dart
@@ -1,4 +1,4 @@
 class Constant {
   static const btcRetrievalUrl = 'https://api.coindesk.com/v1/bpi/currentprice.json';
-  static const btcRetrievalInterval = 5;
+  static const btcRetrievalInterval = 30;
 }
